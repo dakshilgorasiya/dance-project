9e2f29e7df368848eaf7dd06b1f4a82eb327ca1d
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-01-02 21:54:10 +0530

    01_loop.js added

---
+++ 05_iterations/01_loop.js
@@ -0,0 +1,32 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+
+// }
+
+let myArray = ["flash", "batman", "superman"];
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i === 5) {
+//         console.log("5 found");
+//         break;
+//     }
+//     console.log(i);
+// }
+
+for (let i = 0; i <= 20; i++) {
+    if (i === 5) {
+        console.log("5 found");
+        continue;
+    }
+    console.log(i);
+}
